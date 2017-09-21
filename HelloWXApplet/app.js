e51386b6fb3b9c924eec45cf046d17c5b3ce8d2a
huangxiong <huangxionging@qq.com>
2017-09-21 19:08:10 +0800

    更新

--- HelloWXApplet/app.js
+++ HelloWXApplet/app.js
@@ -16,16 +16,40 @@ App({
 
     // 将数组存入本地存储块
     wx.setStorageSync('logs', logs)
+    // this.getUserInfo()
   },
   /**
    * 获取用户信息, 在 Index.js 中会用到
    * cb 意思是 callback 回调函数
    */
-  getUserInfo: function(cb) {
+  getUserInfo: function() {
     var that = this
-    if(this.globalData.userInfo) {
-      // 判断
-    }
+      // 调用登录借口
+      wx.login({
+        success: res => {
+          console.log('登录成功')
+          console.log(res)
+        }
+      })
+      // 获取用户信息
+      wx.getSetting({
+        success: res => {
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权, 可以直接调用 getUserInfo, 不会弹框
+            wx.getUserInfo({
+              success: res => {
+                // 可以将 res 发给后台解码出 unionId
+                this.globalData.userInfo = res.userInfo
+                // 由于 getUserInfo 是网络请求, 可能会在Page. onLoad 之后才返回
+                // 所以此处假如 callback
+                if (this.userInfoReadyCallback) {
+                  this.userInfoReadyCallback(res)
+                }
+              }
+            })
+          }
+        }
+      })
   },
 
   /**
--- HelloWXApplet/pages/index/index.js
+++ HelloWXApplet/pages/index/index.js
@@ -1,26 +1,76 @@
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    motto: 'hello world',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
     
+  },
+
+  bindViewTap: function() {
+    console.log("2222")
+    // var that = this
+    // app.getUserInfo(function (userInfo) {
+    //   that.setData({
+    //     userInfo: userInfo
+    //   })
+    // })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: app.globalData.userInfo,
+          hasUserInfo: true
+        })
+      } 
+    } else { 
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+    var that = this
+    app.getUserInfo(function (userInfo) {
+      that.setData({
+        userInfo: userInfo
+      })
+    })
   },
-
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
   /**
    * 生命周期函数--监听页面显示
    */
@@ -61,5 +111,16 @@ Page({
    */
   onShareAppMessage: function () {
     
+  },
+
+  submit: function() {
+    console.log("提交")
+    wx.request({
+      url: "https://www.baidu.com",
+      success: res => {
+        console.log(res.data)
+
+      }
+    })
   }
 })
