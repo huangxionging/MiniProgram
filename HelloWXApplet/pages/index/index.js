3625460e818718c69d13772cef7b5c6232a8fe4f
huangxiong <huangxionging@163.com>
2017-09-26 21:26:19 +0800

    更新

--- HelloWXApplet/pages/index/index.js
+++ HelloWXApplet/pages/index/index.js
@@ -10,10 +10,10 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
-    
+
   },
 
-  bindViewTap: function() {
+  bindViewTap: function () {
     console.log("2222")
     // var that = this
     // app.getUserInfo(function (userInfo) {
@@ -38,8 +38,8 @@ Page({
           userInfo: app.globalData.userInfo,
           hasUserInfo: true
         })
-      } 
-    } else { 
+      }
+    } else {
       wx.getUserInfo({
         success: res => {
           app.globalData.userInfo = res.userInfo
@@ -75,45 +75,45 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   },
 
-  submit: function() {
+  submit: function () {
     console.log("提交")
     wx.request({
       url: "https://www.baidu.com",
