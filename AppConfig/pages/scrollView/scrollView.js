816d84aff37d3f8a39d90a78f28c6403cb13ae79
huangxiong <huangxionging@qq.com>
2017-09-29 18:49:41 +0800

    更新

--- AppConfig/pages/scrollView/scrollView.js
+++ AppConfig/pages/scrollView/scrollView.js
@@ -1,4 +1,5 @@
 // pages/scrollView/scrollView.js
+var orders = ['red', 'green', 'blue', 'white']
 Page({
 
   /**
@@ -6,7 +7,7 @@ Page({
    */
   data: {
     toView: 'green',
-    scrollTop: 100
+    scrollTop: 200
   },
 
   /**
@@ -68,10 +69,31 @@ Page({
     console.log(e)
   },
   scroll: function(e){
-    console.log(e)
-    console.log(123)
+    // console.log(e)
+    // console.log(123)
   },
   lower: function(e) {
-    console.log(e)
+    // console.log(e)
+    // console.log(124)
+  },
+  tap: function() {
+    console.log(this.data['scrollTop'])
+    var index = orders[Math.floor(Math.random() * 5)]
+    this.setData({
+      toView: index
+    })
+  },
+  tapMove: function (e) {
+    console.log(this.data['scrollTop'])
+    var current = this.data['scrollTop']
+    if (current >= 300) {
+      console.log(2222)
+      current = 0
+    } else {
+      current += 10
+    }
+    this.setData({
+      scrollTop: current
+    })
   }
 })
