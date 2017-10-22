cfc5e3f1fee29d48b7c43882cb6cb50e247c8ecb
huangxiong <huangxionging@163.com>
2017-10-23 00:25:02 +0800

    更新

--- DriftTail/app.js
+++ DriftTail/app.js
@@ -6,7 +6,16 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+    console.log('ddd')
 
+    var loginState = wx.getStorageSync('state')
+    if (loginState) {
+      wx.redirectTo({
+        url: '../../pages/binding/binding',
+      })
+    } else {
+      wx.setStorageSync('state', true)
+    }
     // 登录
     wx.login({
       success: res => {
@@ -17,6 +26,7 @@ App({
     // 获取用户信息
     wx.getSetting({
       success: res => {
+        console.log(res.authSetting)
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
@@ -29,6 +39,22 @@ App({
               if (this.userInfoReadyCallback) {
                 this.userInfoReadyCallback(res)
               }
+            }
+          })
+        } else {
+          wx.authorize({
+            scope: 'userInfo',
+            success: res => {
+
+            }
+          })
+          wx.getUserInfo({
+            withCredentials: true,
+            success: res => {
+              console.log(res.userInfo)
+            },
+            fail: res => {
+              console.log(res)
             }
           })
         }
