8179c4de7fb3612567edd174c45046f113dbb20d
huangxiong <huangxionging@163.com>
2017-11-08 23:50:40 +0800

    更新
    更新

--- AssistantBrushingGame/pages/contest/contestUserDetail/contestUserDetail.js
+++ AssistantBrushingGame/pages/contest/contestUserDetail/contestUserDetail.js
@@ -1,59 +1,15 @@
-// pages/contest/contestUser/contestUser.js
+// pages/contest/contestUserDetail/contestUserDetail.js
 const loginManager = require('../../../manager/loginManager.js')
 const baseWechat = require('../../../utils/baseWeChat.js')
 const baseURL = require('../../../utils/baseURL.js')
 const baseTool = require('../../../utils/baseTool.js')
-
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    loadingDone: true,
-    hasData: true,
-    dataList: [
-      {
-        id: 1,
-        name: '林🐔',
-      },
-      {
-        id: 2,
-        name: '林🐔',
-      },
-      {
-        id: 3,
-        name: '林🐔',
-      },
-      {
-        id: 4,
-        name: '林🐔',
-      },
-      {
-        id: 5,
-        name: '林🐔',
-      },
-      {
-        id: 6,
-        name: '林🐔',
-      },
-      {
-        id: 7,
-        name: '林🐔',
-      },
-      {
-        id: 8,
-        name: '林🐔',
-      },
-      {
-        id: 9,
-        name: '林🐔',
-      },
-      {
-        id: 10,
-        name: '林🐔',
-      },
-    ],
+  
   },
 
   /**
@@ -67,49 +23,48 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+  
   },
-  addContestUser: () => {
-    wx.navigateTo({
-      url: '../addContestUser/addContestUser',
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
-    })
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
   }
 })
