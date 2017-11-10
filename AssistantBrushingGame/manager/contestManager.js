c5b72e4fa8b45db15935305c258d70fbdea9ae96
huangxiong <huangxionging@qq.com>
2017-11-10 19:11:27 +0800

    更新

--- AssistantBrushingGame/manager/contestManager.js
+++ AssistantBrushingGame/manager/contestManager.js
@@ -2,3 +2,25 @@ const baseWechat = require('../utils/baseWeChat.js')
 const baseURL = require('../utils/baseURL.js')
 const baseTool = require('../utils/baseTool.js')
 const baseApiList = require('../utils/baseApiList.js')
+
+function addContestUser(name = '', brushingMethodId = '') {
+  return new Promise((resolve, reject) => {
+    var url = baseURL.baseDomain + baseURL.basePath + baseApiList.addContestUser
+    var data = {
+      name: name,
+      brushingMethodId: brushingMethodId,
+      memberId: baseTool.valueForKey('memberId')
+    }
+    wx.request({
+      url: url,
+      data: data,
+      success: resolve,
+      fail: reject,
+      complete: function(res) {},
+    })
+  })
+}
+
+module.exports = {
+  addContestUser: addContestUser
+}
--- AssistantBrushingGame/pages/contest/addContestUser/addContestUser.js
+++ AssistantBrushingGame/pages/contest/addContestUser/addContestUser.js
@@ -1,17 +1,44 @@
 // pages/contest/addContestUser/addContestUser.js
-Page({
+const app = getApp()
+const loginManager = require('../../../manager/loginManager.js')
+const baseWechat = require('../../../utils/baseWeChat.js')
+const baseURL = require('../../../utils/baseURL.js')
+const baseTool = require('../../../utils/baseTool.js')
+const contestManager = require('../../../manager/contestManager.js')
 
+var select = true
+var contestUserName = ''
+var item = {
+  isNext: false, 
+  selects: [
+    {
+      selectButton: 'userInfo-brush-select-item',
+      title: '标准巴氏刷牙法 (6岁以上)',
+      id: 1,
+      select: true
+    },
+    {
+      selectButton: 'userInfo-brush-select-item',
+      title: '圆弧刷牙法 (6岁以下)',
+      id: 2,
+      select: false
+    }
+  ]
+}
+
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-  
+    item: item
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    
   },
 
   /**
@@ -39,7 +66,6 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
   },
 
   /**
@@ -61,5 +87,56 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  selectClick: function(e) {
+    var that = this
+    // baseTool.print(e)
+    select = 2 - e.target.id
+    baseTool.print(select)
+    baseTool.print(item)
+    item.selects[0].select = select
+    item.selects[1].select = !select
+    baseTool.print(item)
+    // 终于渲染成功了
+    that.setData({
+      item: item
+    })
+  },
+  userInfoSave: function() {
+    var that = this
+    baseTool.print(contestUserName)
+    if (contestUserName == '') {
+      wx.showModal({
+        title: '提示',
+        content: '未输入参赛者名字',
+        showCancel: false,
+        confirmText: '确定',
+        success: function(res) {},
+        fail: function(res) {},
+        complete: function(res) {},
+      })
+      return
+    }
+
+    item.isNext = true
+    // 终于渲染成功了
+    that.setData({
+      item: item
+    })
+
+    var brushMethod = 'a002c7680a5f4f8ea0b1b47fa3f2b947'
+    if(!select) {
+      brushMethod = '6827c45622b141ef869c955e0c51f9f8'
+    }
+
+    contestManager.addContestUser(contestUserName, brushMethod).then(res => {
+      baseTool.print(res)
+    }).catch(res => {
+      baseTool.print(res)
+    })
+  },
+  getInputUserName: function (e){
+    baseTool.print(e)
+    contestUserName = e.detail.value
   }
 })
