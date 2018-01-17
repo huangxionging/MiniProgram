dadd1ba89bbac1db31cb0be291fd6eedcbd52c41
huangxiong <huangxionging@qq.com>
2018-01-17 20:44:06 +0800

    更新

--- AssistantBrushingGame/pages/customerManger/classDetail/classDetail.js
+++ AssistantBrushingGame/pages/customerManger/classDetail/classDetail.js
@@ -1,11 +1,116 @@
 // pages/customerManger/classDetail/classDetail.js
+const app = getApp()
+const contestManager = require('../../../manager/contestManager.js')
+const baseWechat = require('../../../utils/baseWeChat.js')
+const baseURL = require('../../../utils/baseURL.js')
+const baseTool = require('../../../utils/baseTool.js')
+const baseMessageHandler = require('../../../utils/baseMessageHandler.js')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    days: 0,
+    loadDone: false,
+    selectItemIndex: 1,
+    itemList: [
+      {
+        itemName: '个人星球',
+        itemMenuId: 'detail-nomarl',
+        select: false,
+        id: 0
+      },
+      {
+        itemName: '团体星球',
+        itemMenuId: 'detail-select',
+        select: true,
+        id: 1
+      },
+      {
+        itemName: '家庭星球',
+        itemMenuId: 'detail-nomarl',
+        select: false,
+        id: 2
+      }
+    ],
+    personalList: [
+      {
+        score: 98.00,
+        name: 'Umiy',
+        imageUrl: 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLard73s4fMYN22P07rbGOPnVSNcywficjBSJUsLqxLffMwEKlkjdKq3MAibUuicmwHF2E3pN9sPF2icg/0',
+        userId: 1,
+        color: '#ffb9e0'
+      },
+      {
+        score: 97.0,
+        name: 'Umidy',
+        imageUrl: 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLard73s4fMYN22P07rbGOPnVSNcywficjBSJUsLqxLffMwEKlkjdKq3MAibUuicmwHF2E3pN9sPF2icg/0',
+        userId: 2,
+        color: '#fe6941'
+      },
+      {
+        score: 96.0,
+        name: 'Umidsy',
+        imageUrl: 'https://www.baidu.com/img/bd_logo1.png',
+        userId: 3,
+        color: '#2cabee'
+      },
+      {
+        score: 95.0,
+        name: '黄雄',
+        imageUrl: 'https://www.baidu.com/img/bd_logo1.png',
+        userId: 4
+      },
+      {
+        score: 92.0,
+        name: '黄的雄',
+        imageUrl: 'https://www.baidu.com/img/bd_logo1.png',
+        userId: 5
+      }
+    ],
+    groupList: [
+      {
+        blueName: '我爱你一生一世哦哦',
+        blueUrl: 'https://www.baidu.com/img/bd_logo1.png',
+        blueScore: 97.00,
+        isBlueCaptain: true,
+        redName: 'UimdssiY',
+        redUrl: 'https://www.baidu.com/img/bd_logo1.png',
+        redScore: 97.00,
+        isRedCaptain: true
+      },
+      {
+        blueName: 'UimiY',
+        blueUrl: 'https://www.baidu.com/img/bd_logo1.png',
+        blueScore: 97.00,
+        isBlueCaptain: false,
+        redName: 'UimiY',
+        redUrl: 'https://www.baidu.com/img/bd_logo1.png',
+        redScore: 97.00,
+        isRedCaptain: false
+      },
+      {
+        blueName: 'UimiY',
+        blueUrl: 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLard73s4fMYN22P07rbGOPnVSNcywficjBSJUsLqxLffMwEKlkjdKq3MAibUuicmwHF2E3pN9sPF2icg/0',
+        blueScore: 97.00,
+        isBlueCaptain: false,
+        redName: 'UimiY',
+        redUrl: 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLard73s4fMYN22P07rbGOPnVSNcywficjBSJUsLqxLffMwEKlkjdKq3MAibUuicmwHF2E3pN9sPF2icg/0',
+        redScore: 97.00,
+        isRedCaptain: false
+      },
+      {
+        blueName: 'UimiY',
+        blueUrl: 'https://www.baidu.com/img/bd_logo1.png',
+        blueScore: 97.00,
+        isBlueCaptain: false,
+        redName: 'UimiY',
+        redUrl: 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLard73s4fMYN22P07rbGOPnVSNcywficjBSJUsLqxLffMwEKlkjdKq3MAibUuicmwHF2E3pN9sPF2icg/0',
+        redScore: 97.00,
+        isRedCaptain: false
+      }
+    ]
   },
 
   /**
@@ -62,5 +167,28 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  selectClick: function(data) {
+    baseTool.print(data)
+    var that = this
+    var indicate = data.currentTarget.dataset.index
+    var itemList = that.data.itemList
+    for (var index = 0; index < itemList.length; ++index) {
+      if (index == indicate) {
+        itemList[index].itemMenuId = 'detail-select'
+        itemList[index].select = true
+      } else {
+        itemList[index].itemMenuId = 'detail-normal'
+        itemList[index].select = false
+      }
+    }
+    that.setData({
+      selectItemIndex: indicate,
+      itemList: itemList
+    })
+  },
+  personalClick: function(e){
+
   }
+
 })
