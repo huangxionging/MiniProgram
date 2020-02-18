1315864e1000d645a962a62ec1ace5812ebd5021
黄雄 <huangxionging@163.com>
2020-02-18 20:17:05 +0800

    更新
     1. 趋势

---
+++ OtimeSmart/pages/trend/trendIndex/trendIndex.js
@@ -0,0 +1,77 @@
+// pages/trend/trendIndex/trendIndex.js
+const baseTool = require('../../../utils/baseTool.js')
+const baseNetLinkTool = require('../../../utils/baseNetLinkTool.js')
+const baseMessageHandler = require('../../../utils/baseMessageHandler.js')
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    actionSelect: 1
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  actionSlectClick: function(e) {
+    let that = this
+    let index = parseInt(e.currentTarget.dataset.index)
+    that.setData({
+      actionSelect: index
+    })
+    baseTool.print(e)
+  }
+})
